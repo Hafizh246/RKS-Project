372e6bea8b340feb614e52e669c4c94db5d6999e
Hafizh246 <48192315+Hafizh246@users.noreply.github.com>
2021-01-25 11:06:46 +0700

    Index after login
    the account is not connected to the database so only output my name which is Achmad Nur Hafizh

---
+++ index2.php
@@ -0,0 +1,65 @@
+<?php
+  include 'connect.php';
+?>
+
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>HOME</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<style type="text/css">
+			body{
+        background-color: green;
+      }
+			.container-fluid{
+				background-color: rgba(0,0,0,0.5);
+    			text-align: center;
+    			height: 100%;
+
+    		}
+    		div.gallery {
+    			margin: 10px;
+    			border: 1px solid #ccc;
+    			float: left;
+    			width: 180px;
+  			}
+  			div.gallery:hover {
+   				border: 1px solid #777;
+    			border-color: green;
+    			color: green;
+  			}
+  			div.gallery img {
+    			width: 100%;
+    			height: 100%;
+  			}
+  			footer{
+    		position: fixed;
+    		left: 0;
+    		bottom: 0;
+    		width: 100%;
+    		height: 5%;
+        background-color: rgba(0,0,0,0.5);
+    		color: black;
+    		text-align: center;
+    		}
+		</style>
+	</head>
+	<body>
+    <div class="container-fluid">
+        <h1 style="color: white">Welcome to Assignment Collector <br> Achmad Nur Hafizh</h1>
+    </div>
+    <nav class="navbar navbar-expand-lg" style="background-color: rgba(0,0,0,0.8)" >
+      <a class="navbar-brand" href="index2.php">HOME</a>
+	<a class="navbar-brand" href="collectassig.php">COLLECT ASSIGNMENT</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+		<li class="nav-item">
+		<a Class="nav-link" href="index.php"> LOGOUT </a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+ </div>
+   
+	</body>
+	</html>		
