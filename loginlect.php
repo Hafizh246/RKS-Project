ad0be7659fd219371d7c929058ec5ef6237022bf
Hafizh246 <48192315+Hafizh246@users.noreply.github.com>
2021-01-25 11:32:03 +0700

    login for lecturer

---
+++ loginlect.php
@@ -0,0 +1,49 @@
+<?php
+	if(isset($_POST["submit"])){
+		if($_POST["username"] == "lecturer" && $_POST["password"] == "lecturer") {
+			echo '<script> alert("Success"); </script>'; 
+			header("index.php"); 
+		} 
+		
+		else {
+			echo '<script> alert("You Enter Wrong Credential"); </script>';
+		}
+	}
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<body>
+
+<form method="POST" action="index3.php"><table align="center" bgcolor="#7FFFD4" cellpadding="5px">
+	<tr>
+		<th colspan="2" align="center">Login</th>
+	</tr>
+	<tr>
+		<td>
+			Username	
+		</td>
+		<td>
+			Password
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<input type="Username" name="username" required>
+		</td>
+		<td>
+			<input type="password" name="password" required>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<button type="submit" name="submit" class="btn btn-info btn-xs" >Login</button>
+		</td>
+	</tr>
+</table></form>
+</body>
+</html>
